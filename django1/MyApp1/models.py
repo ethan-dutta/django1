94687461fa128d5a9868b3748731afec4b285400
Ethan Dutt <2681401@schoolsnet.act.edu.au>
2026-03-06 11:46:57 +1100

    sorry pete

--- django1/MyApp1/models.py
+++ django1/MyApp1/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class teacher (models.Model):
-    tch_id=models.IntegerField(primary_key=true)
     Name = models.CharField(max_length=25)
     Area = models.CharField(max_length=30)
     def __str__(self):
